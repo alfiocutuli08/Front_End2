0ff20cf653c8060c877b962cd21cefa66ed7d849
antoninoleotta <antonino.leotta05@gmail.com>
2026-05-13 17:22:48 +0200

    Progetto Giusto e Bestemmie

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import SkillProfilePage from "./pages/profiloPersonale";
 import ProfiloPubblico from "./pages/profiloPubblico";
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
+import { PaginaRicercaPage } from "./pages/PaginaRicercaPage";
+import { CardPrincipalePage } from "./pages/CardPrincipalePage";
 import './App.css'
 
 function App() {
@@ -8,11 +12,14 @@ function App() {
     <>
        <BrowserRouter>
         <Routes>
-          <Route path="profile">
+          <Route path="/">
             <Route index element={<SkillProfilePage />} />
             <Route path="public" element={<ProfiloPubblico />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegisterPage />} />
+            <Route path="search" element={<PaginaRicercaPage />} />
+            <Route path="card" element={<CardPrincipalePage />} />
           </Route>
-          <Route path="/" element={<ProfiloPubblico />} />
         </Routes>
       </BrowserRouter>
     </>
