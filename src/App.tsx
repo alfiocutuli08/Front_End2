63bd7b0c53b8fcacca29209eb2451c7450c245c3
Alfio <alfiocutuli08@gmail.com>
2026-05-13 15:17:03 +0200

    paginaricerca

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import { CardPrincipalePage } from "./pages/CardPrincipalePage";
+import { PaginaRicercaPage } from "./pages/PaginaRicercaPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CardPrincipalePage />} />
+        <Route path="/paginaricerca" element={<PaginaRicercaPage />} />
       </Routes>
     </BrowserRouter>
   );
